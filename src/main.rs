8886e7b4705a48939961d39994746f375bf89ae1
Wlegby <gigamullman@gmail.com>
2026-04-07 21:54:49 +0200

    made the movement generating code, found an issue in the remove borders function, it removes all border, not just where the piece move ends (if a rook is in a corner all of it's moves are removed)

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,19 @@ use qol::*;
 
 fn main() {
     let mut state = State::default();
+    dbg!(pext(state.all_pieces, remove_border(EMPTY_PSEUDO_ROOK[0])));
+
+    display_bit_board(&remove_border(EMPTY_PSEUDO_ROOK[0]));
+
+    display_bit_board(&get_legal_move_board(0, PType::Rook, true, &state));
+
+    display_bit_board(
+        &BLOCKED_ROOK[0][pext(state.all_pieces, remove_border(EMPTY_PSEUDO_ROOK[0])) as usize],
+    );
+
+    let all_moves = state.get_all_moves();
+
+    // for _move in all_moves {
+    //     display_move(_move);
+    // }
 }
